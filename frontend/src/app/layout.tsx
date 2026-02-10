de5309c95c23495497fbd9a82e05fb194d21749c
David Palisse <palisse.david44@gmail.com>
2026-02-10 19:57:07 +0100

    add website link description

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,6 +13,31 @@ export const metadata: Metadata = {
   icons: {
     icon: '/monurba-favicon.png',
   },
+  metadataBase: new URL('https://www.mon-urba.fr'),
+  openGraph: {
+    title: 'mon-urba : simplifier vos démarches de construction',
+    description:
+      "Application d'aide à la déclaration de travaux en France. Déterminez le type d'autorisation nécessaire et obtenez la liste des documents requis.",
+    url: 'https://www.mon-urba.fr',
+    siteName: 'mon-urba',
+    locale: 'fr_FR',
+    type: 'website',
+    images: [
+      {
+        url: '/monurba-logo.png',
+        width: 1200,
+        height: 240,
+        alt: 'mon-urba',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'mon-urba : simplifier vos démarches de construction',
+    description:
+      "Application d'aide à la déclaration de travaux en France. Déterminez le type d'autorisation nécessaire et obtenez la liste des documents requis.",
+    images: ['/monurba-logo.png'],
+  },
 };
 
 export default function RootLayout({
